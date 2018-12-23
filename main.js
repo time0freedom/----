d8235de6f30dad4b45b3c4e67440dc809d75b03d
timeOfreedom <673462121@qq.com>
2018-12-23 21:56:18 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function slideAdd(){
+    document.getElementById('slideShower').style.width = 100 +'%';
+}
+
+function slideSub(){
+    document.getElementById('slideShower').style.width = 10 +'%';
+}
+
